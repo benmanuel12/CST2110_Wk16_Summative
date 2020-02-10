33ede417c943457b52217dafee25b03a29bc9a58
Benjamin Manuel <contact.ben.manuel@gmail.com>
2020-02-10 21:22:32 +0000

    added to task 1

--- Task1/src/ApplicationRunner.java
+++ Task1/src/ApplicationRunner.java
@@ -6,16 +6,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ApplicationRunner {
-    
+
     static ArrayList<Trader> traders = new ArrayList<>();
 
     public static void main(String[] args) {
         readFile();
-        for (Trader trader : traders){
-            System.out.println(trader.toString());
-        }
+        menu();
+
     }
 
     static void readFile() {
@@ -23,7 +23,7 @@ public class ApplicationRunner {
         try {
             BufferedReader br = new BufferedReader(new FileReader(dataFile));
             String line;
-            
+
             while ((line = br.readLine()) != null) {
                 String[] array = line.split(":");
                 Trader newTrader = new Trader(array[0], array[1], array[2], Integer.parseInt(array[3]), Double.parseDouble(array[4]), array[5]);
@@ -36,4 +36,67 @@ public class ApplicationRunner {
         }
     }
 
+    static void menu() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("List traders.......1");
+        System.out.println("Select trader .....2");
+        System.out.println("Search locations...3");
+        System.out.println("Search services....4");
+        System.out.println("Exit...............0");
+        System.out.println("");
+        System.out.print("Enter choice:>");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                listTraders();
+                menu();
+            case 2:
+                selectTrader();
+                menu();
+            case 3:
+                searchLocations();
+                menu();
+            case 4:
+                searchServices();
+                menu();
+            case 0:
+                System.exit(0);
+            default:
+                menu();
+
+        }
+    }
+
+    static String rightpad(String text, int length) {
+        return String.format("%-" + length + "." + length + "s", text);
+    }
+
+    static void listTraders() {
+        int id = 1;
+        
+        System.out.println("ID " + rightpad("Company name", 20) + rightpad("Location", 20) + rightpad("Services Offered", 50));
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        for (Trader trader : traders) {
+            StringBuilder str = new StringBuilder(93);
+            str.append(rightpad(String.valueOf(id), 3));
+            str.append(rightpad(trader.getCompanyName(), 25));
+            str.append(rightpad(trader.getLocation(), 20));
+            str.append(rightpad(trader.getServices(), 70));
+            System.out.println(str.toString());
+            id++;
+        }
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    static void selectTrader() {
+
+    }
+
+    static void searchLocations() {
+
+    }
+
+    static void searchServices() {
+
+    }
 }
