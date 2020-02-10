c33ebdf3ff1f6541f5b0a8e29f627a224ae1d5f7
Benjamin Manuel <contact.ben.manuel@gmail.com>
2020-02-10 15:16:50 +0000

    Initial Commit

---
+++ Task1/src/ApplicationRunner.java
@@ -0,0 +1,14 @@
+
+import java.io.File;
+
+
+public class ApplicationRunner {
+    
+    public static void main(String[] args) {
+        
+        String dataFile = System.getProperty("user.dir") + File.separator + "traders.txt";
+        
+
+    }
+    
+}
---
+++ Task3/src/ApplicationRunner.java
@@ -0,0 +1,17 @@
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
+public class ApplicationRunner extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+                
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+     
+}
